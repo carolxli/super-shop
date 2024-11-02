0068999af32ba48ef6ff98debf6300b9664e626e
Maria Carolina Cordeiro <94635867+carolxli@users.noreply.github.com>
2024-11-02 00:09:38 -0300

    Backend produto finalizado-MC

--- backend/controllers/produto.js
+++ backend/controllers/produto.js
@@ -1,7 +1,7 @@
 import {db} from "../db.js";
 
 export const getProdutos = async (req, res) => {
-    const q = `SELECT *FROM Produto`;
+    const q = `SELECT * FROM "Produto"`;
     db.query(q, (err, data) => {
         if (err) return res.json(err);
         return res.status(200).json(data);
@@ -9,18 +9,18 @@ export const getProdutos = async (req, res) => {
 }
 
 export const postProdutos = (req, res) => {
-    const q = `INSERT INTO Produto (
-        sku,
-        descricao,
-        valor_custo,
-        valor_venda,
-        estoque_min,
-        estoque_atual,
-        status,
-        Fornecedor_idFornecedor,
-        Fornecedor_Pessoa_idPessoa,
-        Marca_idMarca,
-        Categoria_idCategoria
+    const q = `INSERT INTO "Produto" (
+        "sku",
+        "descricao",
+        "valor_custo",
+        "valor_venda",
+        "estoque_min",
+        "estoque_atual",
+        "status",
+        "Fornecedor_idFornecedor",
+        "Fornecedor_Pessoa_idPessoa",
+        "Marca_idMarca",
+        "Categoria_idCategoria"
     ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`;
 
     const values = [
@@ -34,7 +34,7 @@ export const postProdutos = (req, res) => {
         req.body.Fornecedor_idFornecedor,
         req.body.Fornecedor_Pessoa_idPessoa,
         req.body.Marca_idMarca,
-        req.body.Categoria_idCategoria
+        req.body.Categoria_idCategoria,
     ];
 
     db.query(q, values, (insertErr) => {
@@ -48,19 +48,19 @@ export const postProdutos = (req, res) => {
 };
 
 export const updateProdutos = (req, res) => {
-    const q = `UPDATE Produto SET 
-        sku = $1,
-        descricao = $2,
-        valor_custo = $3,
-        valor_venda = $4,
-        estoque_min = $5,
-        estoque_atual = $6,
-        status = $7,
-        Fornecedor_idFornecedor = $8,
-        Fornecedor_Pessoa_idPessoa = $9,
-        Marca_idMarca = $10,
-        Categoria_idCategoria = $11
-    WHERE idProduto = $12`;
+    const q = `UPDATE "Produto" SET 
+        "sku" = $1,
+        "descricao" = $2,
+        "valor_custo" = $3,
+        "valor_venda" = $4,
+        "estoque_min" = $5,
+        "estoque_atual" = $6,
+        "status" = $7,
+        "Fornecedor_idFornecedor" = $8,
+        "Fornecedor_Pessoa_idPessoa" = $9,
+        "Marca_idMarca" = $10,
+        "Categoria_idCategoria" = $11
+    WHERE "idProduto" = $12`;
 
     const values = [
         req.body.sku,
@@ -74,10 +74,9 @@ export const updateProdutos = (req, res) => {
         req.body.Fornecedor_Pessoa_idPessoa,
         req.body.Marca_idMarca,
         req.body.Categoria_idCategoria,
-        req.params.idProduto, // Assumindo que o ID do produto é passado como parâmetro na URL
     ];
 
-    db.query(q,[values, req.params.idProduto], (err) => {
+    db.query(q,[...values, req.params.idProduto], (err) => {
         if (err) {
             return res.json(err);
         }
@@ -86,7 +85,7 @@ export const updateProdutos = (req, res) => {
 };
 
 export const deleteProdutos = (req, res) => {
-    const q = `DELETE FROM Produto WHERE \"idProduto\" = $1`;
+    const q = `DELETE FROM "Produto" WHERE \"idProduto\" = $1`;
 
     db.query(q, [req.params.idProduto], (err) => {
         if (err) {
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,12 @@
 import express from "express";
-import routerProd from "./routes/RtProduto";
+import routerProd from "./routes/rtProduto.js";
 import cors from "cors";
-
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/produtos", routerProd);
+app.use("/Produto", routerProd);
 
 app.listen(8800);
