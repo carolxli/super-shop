b479951a139cf0ce4a67882cbd6dc4997d4b1432
gcamposl <gcamposlopes@gmail.com>
2025-03-17 19:37:20 -0300

    fix: alinhamento dos elementos na pagina de despesas

--- frontend/src/pages/ListarDespesa.js
+++ frontend/src/pages/ListarDespesa.js
@@ -71,6 +71,7 @@ const ListarDespesa = () => {
   const handleFilterValor = (e) => {
     setFiltroValor(e.target.value);
   };
+
   const despesasFiltradas = despesas.filter((despesa) => {
     const matchDescricao = despesa.descricao
       .toLowerCase()
@@ -95,132 +96,162 @@ const ListarDespesa = () => {
   });
 
   return (
-    <div>
-      <h2>Listar Despesas</h2>
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          gap: "20px",
-          marginBottom: "20px",
-          padding: "10px",
-          backgroundColor: "#f9f9f9",
-          borderRadius: "8px",
-          boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
-        }}
-      >
-        <input
-          type="text"
-          placeholder="Pesquisar por descrição"
-          value={filtroDescricao}
-          onChange={handleFilterDescricao}
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div style={{ maxWidth: "800px", width: "100%" }}>
+        <h2>Listar Despesas</h2>
+        <div
           style={{
-            padding: "8px",
-            border: "1px solid #ddd",
-            borderRadius: "4px",
-            flex: "1",
-          }}
-        />
-        <input
-          type="date"
-          name="inicio"
-          placeholder="Data inicial"
-          value={filtroPeriodo.inicio}
-          onChange={handleFilterPeriodo}
-          style={{
-            padding: "8px",
-            border: "1px solid #ddd",
-            borderRadius: "4px",
-          }}
-        />
-        <input
-          type="date"
-          name="fim"
-          placeholder="Data final"
-          value={filtroPeriodo.fim}
-          onChange={handleFilterPeriodo}
-          style={{
-            padding: "8px",
-            border: "1px solid #ddd",
-            borderRadius: "4px",
-          }}
-        />
-        <select
-          value={filtroStatus}
-          onChange={handleFilterStatus}
-          style={{
-            padding: "8px",
-            border: "1px solid #ddd",
-            borderRadius: "4px",
-            flex: "1",
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "20px",
+            marginBottom: "20px",
+            padding: "10px",
+            backgroundColor: "#f9f9f9",
+            borderRadius: "8px",
+            boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
           }}
         >
-          <option value="">Filtrar por status</option>
-          <option value="Pago">Pago</option>
-          <option value="Pendente">Pendente</option>
-          <option value="Cancelado">Cancelado</option>
-        </select>
-        <input
-          type="number"
-          placeholder="Filtrar por valor máximo"
-          value={filtroValor}
-          onChange={handleFilterValor}
-          style={{
-            padding: "8px",
-            border: "1px solid #ddd",
-            borderRadius: "4px",
-            flex: "1",
-          }}
-        />
-        <Link to="/cadastrarDespesa">
-          <button
+          <input
+            type="text"
+            placeholder="Pesquisar por descrição"
+            value={filtroDescricao}
+            onChange={handleFilterDescricao}
             style={{
-              padding: "8px 16px",
-              backgroundColor: "#87CEEB",
-              color: "#fff",
-              border: "none",
+              display: "block",
+              padding: "8px",
+              border: "1px solid #ddd",
               borderRadius: "4px",
-              cursor: "pointer",
-              transition: "background-color 0.3s",
+              flex: "1",
+            }}
+          />
+          <input
+            type="date"
+            name="inicio"
+            placeholder="Data inicial"
+            value={filtroPeriodo.inicio}
+            onChange={handleFilterPeriodo}
+            style={{
+              padding: "8px",
+              border: "1px solid #ddd",
+              borderRadius: "4px",
+            }}
+          />
+          <input
+            type="date"
+            name="fim"
+            placeholder="Data final"
+            value={filtroPeriodo.fim}
+            onChange={handleFilterPeriodo}
+            style={{
+              padding: "8px",
+              border: "1px solid #ddd",
+              borderRadius: "4px",
+            }}
+          />
+          <select
+            value={filtroStatus}
+            onChange={handleFilterStatus}
+            style={{
+              padding: "8px",
+              border: "1px solid #ddd",
+              borderRadius: "4px",
+              flex: "1",
             }}
           >
-            Cadastrar Nova Despesa
-          </button>
-        </Link>
-      </div>
-      <table border="1" style={{ width: "100%", borderCollapse: "collapse" }}>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Descrição</th>
-            <th>Tipo de Despesa</th>
-            <th>Valor</th>
-            <th>Data</th>
-            <th>Status</th>
-            <th>Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-          {despesasFiltradas.length > 0 ? (
-            despesasFiltradas.map((despesa) => (
-              <tr key={despesa.idDespesa}>
-                <td>{despesa.idDespesa}</td>
-                <td>{despesa.descricao}</td>
-                <td>{despesa.nome_tipo || "N/A"}</td>
-                <td>{despesa.valor}</td>
-                <td>{new Date(despesa.dt_despesa).toLocaleDateString()}</td>
-                <td>{despesa.status}</td>
-                <td>
-                  <Link
-                    to={`/editarDespesa/${despesa.idDespesa}`}
-                    state={{ status: despesa.status }}
-                  >
+            <option value="">Filtrar por status</option>
+            <option value="Pago">Pago</option>
+            <option value="Pendente">Pendente</option>
+            <option value="Cancelado">Cancelado</option>
+          </select>
+          <input
+            type="number"
+            placeholder="Filtrar por valor máximo"
+            value={filtroValor}
+            onChange={handleFilterValor}
+            style={{
+              padding: "8px",
+              border: "1px solid #ddd",
+              borderRadius: "4px",
+              flex: "1",
+            }}
+          />
+          <Link to="/cadastrarDespesa">
+            <button
+              style={{
+                padding: "8px 16px",
+                backgroundColor: "#87CEEB",
+                color: "#fff",
+                border: "none",
+                borderRadius: "4px",
+                cursor: "pointer",
+                transition: "background-color 0.3s",
+              }}
+            >
+              Cadastrar Nova Despesa
+            </button>
+          </Link>
+        </div>
+        <table
+          border="1"
+          style={{
+            width: "100%",
+            borderCollapse: "collapse",
+            marginBottom: "20px",
+          }}
+        >
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Descrição</th>
+              <th>Tipo de Despesa</th>
+              <th>Valor</th>
+              <th>Data</th>
+              <th>Status</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {despesasFiltradas.length > 0 ? (
+              despesasFiltradas.map((despesa) => (
+                <tr key={despesa.idDespesa}>
+                  <td>{despesa.idDespesa}</td>
+                  <td>{despesa.descricao}</td>
+                  <td>{despesa.nome_tipo || "N/A"}</td>
+                  <td>{despesa.valor}</td>
+                  <td>{new Date(despesa.dt_despesa).toLocaleDateString()}</td>
+                  <td>{despesa.status}</td>
+                  <td>
+                    <Link
+                      to={`/editarDespesa/${despesa.idDespesa}`}
+                      state={{ status: despesa.status }}
+                    >
+                      <button
+                        style={{
+                          padding: "6px 12px",
+                          marginRight: "5px",
+                          backgroundColor:
+                            despesa.status === "Pago" ? "#ccc" : "#87CEEB",
+                          color: "#fff",
+                          border: "none",
+                          borderRadius: "4px",
+                          cursor:
+                            despesa.status === "Pago"
+                              ? "not-allowed"
+                              : "pointer",
+                        }}
+                        disabled={despesa.status === "Pago"}
+                      >
+                        Editar
+                      </button>
+                    </Link>
                     <button
+                      onClick={() =>
+                        handleDelete(despesa.idDespesa, despesa.status)
+                      }
                       style={{
                         padding: "6px 12px",
-                        marginRight: "5px",
                         backgroundColor:
-                          despesa.status === "Pago" ? "#ccc" : "#87CEEB",
+                          despesa.status === "Pago" ? "#ccc" : "#FF6347",
                         color: "#fff",
                         border: "none",
                         borderRadius: "4px",
@@ -229,37 +260,19 @@ const ListarDespesa = () => {
                       }}
                       disabled={despesa.status === "Pago"}
                     >
-                      Editar
+                      Deletar
                     </button>
-                  </Link>
-                  <button
-                    onClick={() =>
-                      handleDelete(despesa.idDespesa, despesa.status)
-                    }
-                    style={{
-                      padding: "6px 12px",
-                      backgroundColor:
-                        despesa.status === "Pago" ? "#ccc" : "#FF6347",
-                      color: "#fff",
-                      border: "none",
-                      borderRadius: "4px",
-                      cursor:
-                        despesa.status === "Pago" ? "not-allowed" : "pointer",
-                    }}
-                    disabled={despesa.status === "Pago"}
-                  >
-                    Deletar
-                  </button>
-                </td>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="7">Nenhuma despesa encontrada.</td>
               </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="7">Nenhuma despesa encontrada.</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
