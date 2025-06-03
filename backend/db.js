14838e966855f36e8a72eadd05cfc5d8a5f17fc2
gcamposl <gcamposlopes@gmail.com>
2025-06-02 22:40:10 -0300

    fix: update expense report references in Relatorios component and adjust database configuration

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,19 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-export const db = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: "123",
-  database: "SuperShop",
-  port: 5432,
-});
-
-// vini db config
 // export const db = new Pool({
 //   host: "localhost",
 //   user: "postgres",
-//   password: "postgres",
-//   database: "supershop",
+//   password: "123",
+//   database: "SuperShop",
 //   port: 5432,
 // });
+
+// vini db config
+export const db = new Pool({
+  host: "localhost",
+  user: "postgres",
+  password: "postgres",
+  database: "supershop",
+  port: 5432,
+});
